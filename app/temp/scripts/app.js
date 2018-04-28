ebee39d28ab59e6af0df67b97622e651c68cc5fb
Trevor Habermeyer <thabermeyer@gmail.com>
2018-04-28 15:19:08 -0600

    Add basic calculation functionality

--- app/temp/scripts/app.js
+++ app/temp/scripts/app.js
@@ -9032,245 +9032,42 @@ module.exports = function (regExp, replace) {
 "use strict";
 
 
-// Define UI Variables
+// Listen for submit
 
-var form = document.querySelector('.task-input__form');
-var taskInput = document.querySelector('.task-input__task');
-var taskBtn = document.querySelector('.task-input__btn');
-var filter = document.querySelector('.task-list__filter');
-var taskList = document.querySelector('.task-list__list');
-var clearBtn = document.querySelector('.task-list__btn');
+document.querySelector('.calculator__form').addEventListener('submit', calculateResults);
 
-// Load all event listeners
+function calculateResults(e) {
 
-loadEventListeners();
+    // UI Variables
 
-function loadEventListeners() {
+    var amount = document.getElementById('amount');
+    var interest = document.getElementById('interest');
+    var years = document.getElementById('years');
 
-    // DOM Load event
+    var monthlyPayment = document.getElementById('monthly-payment');
+    var totalPayment = document.getElementById('total-payment');
+    var totalInterest = document.getElementById('total-interest');
 
-    document.addEventListener('DOMContentLoaded', getTasks);
+    var principal = parseFloat(amount.value);
+    var calculatedInterest = parseFloat(interest.value) / 100 / 12;
+    var calculatedPayments = parseFloat(years.value) * 12;
 
-    // Add task event
+    // Compute monthly payments
 
-    form.addEventListener('submit', addTask);
+    var x = Math.pow(1 + calculatedInterest, calculatedPayments);
+    var monthly = principal * x * calculatedInterest / (x - 1);
 
-    // Remove task event
+    if (isFinite(monthly)) {
 
-    taskList.addEventListener('click', removeTask);
-
-    // Clear task event
-
-    clearBtn.addEventListener('click', clearTasks);
-
-    // Filter task event
-
-    filter.addEventListener('keyup', filterTasks);
-}
-
-// Get Tasks from Local Storage
-
-function getTasks() {
-
-    var tasks = void 0;
-
-    if (localStorage.getItem('tasks') === null) {
-
-        tasks = [];
+        monthlyPayment.value = monthly.toFixed(2);
+        totalPayment.value = (monthly * calculatedPayments).toFixed(2);
+        totalInterest.value = (monthly * calculatedPayments - principal).toFixed(2);
     } else {
 
-        tasks = JSON.parse(localStorage.getItem('tasks'));
+        console.log('Please check your numbers');
     }
-
-    tasks.forEach(function (task) {
-
-        // Create li element
-
-        var li = document.createElement('li');
-
-        // Add class
-
-        li.className = 'task-list__list-item';
-
-        // Create text node and append to li
-
-        li.appendChild(document.createTextNode(task));
-
-        // Create new link element
-
-        var link = document.createElement('a');
-
-        // Add class
-
-        link.className = 'task-list__list-item-icon delete-item';
-
-        // Add icon HTML
-
-        link.innerHTML = '<i class="fa fa-remove"></i>';
-
-        // Append link to li
-
-        li.appendChild(link);
-
-        // Append li to ul
-
-        taskList.appendChild(li);
-    });
-}
-
-// Add Task
-
-function addTask(e) {
-
-    if (taskInput.value === '') {
-
-        alert('Add a task');
-    }
-
-    // Create li element
-
-    var li = document.createElement('li');
-
-    // Add class
-
-    li.className = 'task-list__list-item';
-
-    // Create text node and append to li
-
-    li.appendChild(document.createTextNode(taskInput.value));
-
-    // Create new link element
-
-    var link = document.createElement('a');
-
-    // Add class
-
-    link.className = 'task-list__list-item-icon delete-item';
-
-    // Add icon HTML
-
-    link.innerHTML = '<i class="fa fa-remove"></i>';
-
-    // Append link to li
-
-    li.appendChild(link);
-
-    // Append li to ul
-
-    taskList.appendChild(li);
-
-    // Store in local storage
-
-    storeTaskInLocalStorage(taskInput.value);
-
-    // Clear input
-
-    taskInput.value = '';
 
     e.preventDefault();
-}
-
-// Store Task
-
-function storeTaskInLocalStorage(task) {
-
-    var tasks = void 0;
-
-    if (localStorage.getItem('tasks') === null) {
-
-        tasks = [];
-    } else {
-
-        tasks = JSON.parse(localStorage.getItem('tasks'));
-    }
-
-    tasks.push(task);
-
-    localStorage.setItem('tasks', JSON.stringify(tasks));
-}
-
-// Remove Task
-
-function removeTask(e) {
-
-    if (e.target.parentElement.classList.contains('delete-item')) {
-
-        if (confirm('Are you sure you want to delete this task?')) {
-
-            e.target.parentElement.parentElement.remove();
-
-            // Remove from local storage
-
-            removeTaskFromLocalStorage(e.target.parentElement.parentElement);
-        }
-    }
-}
-
-// Remove from Local Storage
-
-function removeTaskFromLocalStorage(taskItem) {
-
-    var tasks = void 0;
-
-    if (localStorage.getItem('tasks') === null) {
-
-        tasks = [];
-    } else {
-
-        tasks = JSON.parse(localStorage.getItem('tasks'));
-    }
-
-    tasks.forEach(function (task, index) {
-
-        if (taskItem.textContent === task) {
-
-            tasks.splice(index, 1);
-        }
-    });
-
-    localStorage.setItem('tasks', JSON.stringify(tasks));
-}
-
-// Clear All Tasks
-
-function clearTasks() {
-
-    if (confirm('Are you sure you want to clear all tasks?')) {
-
-        while (taskList.firstChild) {
-
-            taskList.removeChild(taskList.firstChild);
-        }
-    }
-
-    clearTasksFromLocalStorage();
-}
-
-// Clear tasks from local storage
-
-function clearTasksFromLocalStorage() {
-
-    localStorage.clear();
-}
-
-// Filter Tasks
-
-function filterTasks(e) {
-
-    var text = e.target.value.toLowerCase();
-
-    document.querySelectorAll('.task-list__list-item').forEach(function (task) {
-
-        var item = task.firstChild.textContent;
-
-        if (item.toLowerCase().indexOf(text) != -1) {
-
-            task.style.display = 'flex';
-        } else {
-
-            task.style.display = 'none';
-        }
-    });
 }
 
 /***/ })
